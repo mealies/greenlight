ff495d0bd98456f3ee88c2f59107f8228e2feb60
Andrew <andrew@thatopsguy.com>
2024-03-03 15:07:55 +0000

    add json tags to movie type
    Add JSON tags to movie trype for better output.

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,12 +3,12 @@ package data
 import "time"
 
 type Movie struct {
-	ID        int64     // Unique integer ID for the movie
-	CreatedAt time.Time // Timestamp for when the movie is added to our database
-	Title     string    // Movie title
-	Year      int32     // Movie release year
-	Runtime   int32     // Movie runtime (in minutes)
-	Genres    []string  // Slice of genres for the movie (romance, comedy, etc.)
-	Version   int32     // The version number starts at 1 and will be incremented
+	ID        int64     `json:"id"`                // Unique integer ID for the movie
+	CreatedAt time.Time `json:"-"`                 // Timestamp for when the movie is added to our database
+	Title     string    `json:"title"`             // Movie title
+	Year      int32     `json:"year,omitempty"`    // Movie release year
+	Runtime   int32     `json:"runtime,omitempty"` // Movie runtime (in minutes)
+	Genres    []string  `json:"genres,omitempty"`  // Slice of genres for the movie (romance, comedy, etc.)
+	Version   int32     `json:"version"`           // The version number starts at 1 and will be incremented
 	// each time the movie information is updated
 }
